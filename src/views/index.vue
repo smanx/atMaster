7d7468f49341227789472d0887dc6ccbc5e439c5
cc <m1@123.com>
2024-03-13 08:41:33 +0800

    save

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,8 @@
 <template>
   <button @click="$router.push({path: 'zc'});">中兴微</button>
   <button @click="$router.push({path: 'asr'});">ASR</button>
+  <button @click="$router.push({path: ''});">展锐（开发中）</button>
+  <p style="opacity: 0.1;">需要先安装对应驱动到电脑，支持基于chromium内核的浏览器，串行端口需要选择AT端口</p>
 </template>
 
 <script>
