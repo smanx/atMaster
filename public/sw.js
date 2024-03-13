60c25e22f2d1e0d434ea911c63c850388b1e411c
zc <zc@zc.com>
2024-03-13 16:19:58 +0800

    save

---
+++ public/sw.js
@@ -0,0 +1,2 @@
+import { precacheAndRoute } from 'workbox-precaching'
+precacheAndRoute(self.__WB_MANIFEST)
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    VitePWA({
+      strategies: 'injectManifest',
+      workbox: {
+        sourcemap: false
+      }
+    }),
   ],
   resolve: {
     alias: {
